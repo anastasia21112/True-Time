9680b0745f04bba0b2e91fc83e7fa3fc1d8ff596
angelalayton <angelalayton1@gmail.com>
2021-03-20 16:08:25 -0600

    task object

---
+++ src/pkgtrue/time/Task.java
@@ -0,0 +1,14 @@
+
+package pkgtrue.time;
+
+public class Task {
+    
+    private String taskName;
+    private Double averageTime;
+    
+    public Task (String name, Double time)
+    {
+        this.taskName = name;
+        this.averageTime = time;
+    }
+}
