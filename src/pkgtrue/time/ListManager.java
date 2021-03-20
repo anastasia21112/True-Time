f64c4ef2603d27d79375474ac20d30bd268d5cea
angelalayton <angelalayton1@gmail.com>
2021-03-20 16:11:17 -0600

    updated list manager

--- src/pkgtrue/time/ListManager.java
+++ src/pkgtrue/time/ListManager.java
@@ -1,34 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pkgtrue.time;
 
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Scanner;
-import java.util.Set;
 public class ListManager {
-
-
-    public static HashMap<String, ArrayList<Task>> listManager;
-
+    
+    //HashMap<String, ArrayList<Task>>
+    //HashMap<listName, arrayList of Tasks> 
+    public static HashMap<String, ArrayList<Task>> listManager = new HashMap<String, ArrayList<Task>>();
+    
     public ListManager()
     {
-        listManager = new HashMap<String, ArrayList<Task>>();
+          
+    }
+    
+    public void addList(String listName)
+    {
+        ArrayList<Task> empty = new ArrayList<Task>();
+        listManager.put(listName, empty);
+    }
+    
+    public void addTask(String listName, String taskName)
+    {
+        
     }
 }
