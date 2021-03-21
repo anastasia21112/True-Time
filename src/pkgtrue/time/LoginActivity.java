6cbbfa34d49ee66d584297de9b7f7f6383ec6021
anastasia21112 <anad221122@gmail.com>
2021-03-20 19:29:32 -0600

    login

--- src/pkgtrue/time/LoginActivity.java
+++ src/pkgtrue/time/LoginActivity.java
@@ -7,13 +7,16 @@ package pkgtrue.time;
 
 import java.awt.BorderLayout;
 import java.awt.Graphics;
+import java.awt.Image;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import layout.TableLayout;
+import javax.swing.*;
 
 /**
  *
@@ -22,27 +25,32 @@ import layout.TableLayout;
 public class LoginActivity extends JFrame{
     
     private JButton loginButton = new JButton("LOGIN");
-    private JTextField usernameField = new JTextField("USERNAME");
-    private JTextField passwordField = new JTextField("PASSWORD");
+    private JTextField usernameField = new JTextField("");
+    private JPasswordField passwordField = new JPasswordField();
+    private JLabel usernameLabel = new JLabel("Username: ");
+    private JLabel passwordLabel = new JLabel("Password: ");
+    private JLabel logoLabel = new JLabel();
     
     public LoginActivity()
     {
         this.setContentPane(new ImagePanel());
-        JPanel loginPanel = new JPanel();
-   
-        loginPanel.setLayout(new TableLayout());
-        loginPanel.setSize(10, 30);
+  
+        logoLabel.setIcon(new ImageIcon(new ImageIcon("images/logo_part_transparent.png").getImage().getScaledInstance(150, 150, Image.SCALE_DEFAULT)));
+         double windowSize[][] = {{90, 150, 130, 20}, // Columns
+            {80, 250, 10, 10, 20, 30, 10, 10, 20, 30, 20}}; // Rows
+        this.setLayout(new TableLayout(windowSize));
         
-        loginPanel.add(usernameField, BorderLayout.NORTH);
-        loginPanel.add(passwordField, BorderLayout.CENTER);
-        loginPanel.add(loginButton, BorderLayout.SOUTH);
+        this.add(logoLabel, "1, 1");
+        this.add(usernameLabel, "1, 2");
+        this.add(usernameField, "1, 4");
+        this.add(passwordLabel, "1, 6");
+        this.add(passwordField, "1, 8");
+        this.add(loginButton, "1, 10");
         
-        loginPanel.setVisible(true);
-        this.setLayout(new BorderLayout());
-        this.add(loginPanel, BorderLayout.SOUTH);
+        
         
         this.setTitle("Login");
-        this.setSize(300, 500);
+        this.setSize(325, 675);
         
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
